f756b7880d0be2c0b38a94265df201bd63bbea51
voixdigitale <cristian@voixdigitale.com>
2023-10-17 14:16:52 +0200

    Initial commit

---
+++ Carte.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeptFamilles {
+    internal class Carte : IEquatable<Carte> {
+        public Membres Membre { get; private set; }
+        public Familles Famille { get; private set; }
+
+        public Carte(Membres membre, Familles famille) {
+            Membre = membre;
+            Famille = famille;
+        }
+
+        public string NomCarte() {
+            return Membre.ToString() + " " + Famille.ToString();
+        }
+
+        public void MontreNom() {
+            ConsoleColor originalFG = Console.ForegroundColor;
+            Console.ForegroundColor = CouleurFamille(Famille);
+            Console.WriteLine(NomCarte());
+            Console.ForegroundColor = originalFG;
+        }
+
+        private ConsoleColor CouleurFamille(Familles famille) => famille switch {
+            Familles.Zombie => ConsoleColor.DarkGreen,
+            Familles.Ninja => ConsoleColor.DarkGray,
+            Familles.Pirate => ConsoleColor.Magenta,
+            Familles.Slime => ConsoleColor.Green,
+            Familles.LoupGarou => ConsoleColor.DarkRed,
+            Familles.RatMutante => ConsoleColor.DarkYellow,
+            Familles.Avocat => ConsoleColor.Red,
+        };
+
+        public bool Equals(Carte? other) {
+            if (other.Membre == Membre && other.Famille == Famille) {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Joueur.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeptFamilles {
+    internal class Joueur {
+        private List<Carte> _mainCartes;
+        private Dictionary<Familles, int> _familles;
+
+        public Joueur() {
+            _mainCartes = new List<Carte>();
+            _familles = new Dictionary<Familles, int>();
+        }
+
+        public void Piocher(int numCartes) {
+            for (int i = 0; i < numCartes; i++) {
+                _mainCartes.Add(Program.JeuCartes.First());
+                Program.JeuCartes.Remove(Program.JeuCartes.First());
+            }
+
+            EvaluerCartes();
+        }
+
+        public void EvaluerCartes() {
+            //Pour chaque famille
+            foreach(Familles famille in Enum.GetValues(typeof(Familles))) {
+                int numMembres = 0;
+
+                //On compte combien de cartes on a de cette famille
+                foreach(Carte carte in _mainCartes) {
+                    if(carte.Famille == famille) {
+                        numMembres++;
+                    }
+                }
+
+                //Et on mémorise
+                _familles.Add(famille, numMembres);
+            }
+
+            //On va les ranger pour lisibilité
+            var listeFamilles = _familles.OrderByDescending(x => x.Value).ToList();
+
+            _familles.Clear();
+            foreach (KeyValuePair<Familles, int> famille in listeFamilles) {
+                _familles.Add(famille.Key, famille.Value);
+            }
+
+        }
+
+        public void DevoilerCartes() {
+            foreach (Carte carte in _mainCartes) {
+                if (carte != null) {
+                    carte.MontreNom();
+                }
+            }
+        }
+
+        public void DebugEvaluation() {
+            Console.WriteLine("\nIci ma réflexion !\n");
+            var asString = string.Join(Environment.NewLine, _familles);
+            Console.WriteLine(asString);
+            var premierChoix = _familles.First();
+            Console.WriteLine($"\nJe vais demander une carte de la famille: {premierChoix.Key.ToString()}");
+        }
+    }
+}
